702f402948645116d55b1fd5e63d7488260c4d63
Folashade <folajoachim@yahoo.co.uk>
2022-06-02 16:19:02 +0100

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "ef338f15103ddb4f3d78cb7b6408d7e7";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Abuja&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
