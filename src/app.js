a73af6e9447ba2ba8c27d2fca8965064230b399b
Folashade <folajoachim@yahoo.co.uk>
2022-06-03 17:22:09 +0100

    added button, geolocation and css

--- src/app.js
+++ src/app.js
@@ -59,6 +59,16 @@ function handleSubmit(event) {
   searchCity(cityInputElement.value);
 }
 
+function showPosition(position) {
+  let apiKey = "ef338f15103ddb4f3d78cb7b6408d7e7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -79,6 +89,9 @@ let celsiusTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
+
+let button = document.querySelector("button");
+button.addEventListener("click", getCurrentPosition);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
